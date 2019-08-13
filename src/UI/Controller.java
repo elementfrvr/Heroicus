dbbec12b9c1292f9b3ee80071d15d4c8997a90a1
Dakota Mitchem <mitchemdakota@gmail.com>
2019-08-13 14:25:57 -0500

    Renamed files and folders

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package UI;
 
 public class Controller {
 }
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Heroicus Agent Management");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
