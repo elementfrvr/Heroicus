c9ac9f307052f5b1cb59b43e3564c86d34e67490
Dakota Mitchem <mitchemdakota@gmail.com>
2019-08-13 14:31:13 -0500

    Added documentation to customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,7 +1,11 @@
+/*This class will hold the new customer data prior to being added to the database
+*/
+
 import java.util.Date;
 import java.math.BigDecimal;
 
 public class Customer {
+    //Customer data
     private String lastName;
     private String firstName;
     private Date policyStart;
@@ -11,6 +15,7 @@ public class Customer {
     private int customerAge;
     private float assetRisk;
 
+    //Constructor
     public Customer(String last, String first, Date start, Date end, BigDecimal coverage, BigDecimal value, int age, float risk){
         lastName=last;
         firstName=first;
@@ -22,6 +27,8 @@ public class Customer {
         assetRisk=risk;
     }
 
+    //TODO
+    //Create function for customer to be added to database
     public void addToDatabase(){
 
     }
