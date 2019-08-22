f81244328c37e7270c95e98607e9c4445488c82b
Dakota Mitchem <mitchemdakota@gmail.com>
2019-08-22 15:01:55 -0500

    Edited login UI

--- src/heroicus/Main.java
+++ src/heroicus/Main.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Heroicus Agent Management");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
