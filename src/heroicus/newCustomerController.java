5ca811f759eb988a78ed4e0a9ff7815b06e57466
Dakota Mitchem <mitchemdakota@gmail.com>
2019-09-03 17:25:09 -0500

    Created new customer controller class

---
+++ src/heroicus/newCustomerController.java
@@ -0,0 +1,25 @@
+package heroicus;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class newCustomerController {
+
+    @FXML
+    private TextField txtCustIncome;
+
+    @FXML
+    private TextField txtCustName;
+
+    @FXML
+    private TextField txtCustAddress;
+
+    @FXML
+    private Label lblFeedback;
+
+    @FXML
+    private Button btnConfirm;
+
+}
